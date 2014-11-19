ba3063116bb6f7f310313b6ae8f83d1c5ee30305
jglrxavpok <jglrxavpok@gmail.com>
2014-11-19 19:16:04 +0100

    Init of Tecbula

---
+++ src/org/craft/tecbula/TecbulaMod.java
@@ -0,0 +1,73 @@
+package org.craft.tecbula;
+
+import java.util.*;
+
+import org.craft.client.*;
+import org.craft.client.gui.*;
+import org.craft.client.gui.widgets.*;
+import org.craft.modding.*;
+import org.craft.modding.events.*;
+import org.craft.modding.events.gui.*;
+import org.craft.tecbula.gui.*;
+import org.slf4j.*;
+
+@Mod(id = "tecbula", author = "jglrxavpok", name = "Tecbula", version = "Secret 0.0.1")
+public class TecbulaMod
+{
+    public static final int TECBULA_BUTTON_ID = 404;
+    private Logger          logger;
+
+    @OurModEventHandler
+    public void onPreInit(ModPreInitEvent evt)
+    {
+        logger = evt.getLogger();
+        logger.info("Tecbula loading...");
+    }
+
+    @OurModEventHandler
+    public void onGuiPostBuilding(GuiBuildingEvent.Post evt)
+    {
+        if(evt.getMenu() instanceof GuiMainMenu)
+        {
+            Gui gui = evt.getMenu();
+            List<GuiWidget> widgets = gui.getAllWidgets();
+            int x = 0;
+            int y = 0;
+            int w = 0;
+            int h = 0;
+            for(GuiWidget widget : widgets)
+            {
+                if(widget.getID() == 2) // 2 is the id of the settings button
+                {
+                    GuiButton settingsButton = (GuiButton) widget;
+                    settingsButton.setWidth(settingsButton.getWidth() / 2 - 10);
+                    x = settingsButton.getX();
+                    y = settingsButton.getY();
+                    w = settingsButton.getWidth();
+                    h = settingsButton.getHeight();
+                    settingsButton.setLocation(x + settingsButton.getWidth() + 20, y);
+                }
+            }
+            GuiButton tecbulaButton = new GuiButton(TECBULA_BUTTON_ID, x, y, w, h, "Tecbula", gui.getFontRenderer());
+            gui.addWidget(tecbulaButton);
+        }
+    }
+
+    @OurModEventHandler
+    public void onActionPerformed(GuiActionPerformedEvent evt)
+    {
+        if(evt.getMenu() instanceof GuiMainMenu)
+        {
+            if(evt.getWidget().getID() == TECBULA_BUTTON_ID)
+            {
+                OurCraft.getOurCraft().openMenu(new GuiTecbula(OurCraft.getOurCraft()));
+            }
+        }
+    }
+
+    @OurModEventHandler
+    public void onPostInit(ModPostInitEvent evt)
+    {
+        logger.info("Tecbula fully loaded!");
+    }
+}
---
+++ src/org/craft/tecbula/gui/GuiTecbula.java
@@ -0,0 +1,196 @@
+package org.craft.tecbula.gui;
+
+import static org.lwjgl.opengl.GL11.*;
+
+import java.io.*;
+import java.util.*;
+
+import com.google.common.collect.*;
+
+import org.craft.blocks.*;
+import org.craft.client.*;
+import org.craft.client.gui.*;
+import org.craft.client.gui.widgets.*;
+import org.craft.client.models.*;
+import org.craft.client.render.*;
+import org.craft.inventory.Stack;
+import org.craft.maths.*;
+import org.craft.resources.*;
+
+public class GuiTecbula extends Gui
+{
+
+    private OffsettedOpenGLBuffer           buff;
+    private float                           yAxis;
+    private float                           xAxis;
+
+    private float                           transX;
+    private float                           transY;
+    private float                           transZ;
+    private float                           zoom;
+    private boolean                         rightMousePressed;
+    private boolean                         middleMousePressed;
+    private Stack                           rootStack;
+    private ModelBase                       currentModel;
+    private HashMap<ModelBox, OpenGLBuffer> buffers;
+    private Texture                         texture;
+
+    public GuiTecbula(OurCraft game)
+    {
+        super(game);
+        buffers = Maps.newHashMap();
+    }
+
+    @Override
+    public boolean requiresMouse()
+    {
+        return true;
+    }
+
+    @Override
+    public void init()
+    {
+        currentModel = new ModelBase();
+
+        rootStack = new Stack(Blocks.log, 1);
+        buff = new OffsettedOpenGLBuffer();
+        buff.setToPlane(-2f, 0, -2f, 5f, 0, 5f);
+        buff.setOffsetToEnd();
+        ModelBox testBox = new ModelBox(0.15f, 1f, 0.15f, 0.70f, 0.70f, 0.70f);
+        testBox.setPixelRatio(11.428f);
+
+        ModelBox testBox2 = new ModelBox(0f, 0f, 0f, 1f, 1f, 1f);
+        testBox2.setPixelRatio(8);
+
+        ModelBox testBox3 = new ModelBox(0.25f, 1.7f, 0.25f, 0.50f, 0.50f, 0.50f);
+        testBox3.setPixelRatio(16f);
+
+        currentModel.addBox(testBox);
+        currentModel.addBox(testBox2);
+        currentModel.addBox(testBox3);
+        try
+        {
+            texture = OpenGLHelper.loadTexture(oc.getAssetsLoader().getResource(new ResourceLocation("ourcraft", "textures/entities/test.png")));
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        buff.clearAndDisposeVertices();
+        addWidget(new GuiLabel(0, 0, 0, "Tecbula menu", oc.getFontRenderer()));
+        addWidget(new GuiButton(1, 0, 20, 200, 40, "Back to main menu", oc.getFontRenderer()));
+    }
+
+    public void actionPerformed(GuiWidget w)
+    {
+        if(w.getID() == 1)
+        {
+            oc.openMenu(new GuiMainMenu(oc));
+        }
+    }
+
+    public void draw(int mx, int my, RenderEngine engine)
+    {
+        drawBackground(mx, my, engine);
+        super.draw(mx, my, engine);
+        engine.begin();
+        engine.switchToPerspective();
+        engine.enableGLCap(GL_ALPHA_TEST);
+        engine.setAlphaFunc(GL_GREATER, 0.0001f);
+        engine.enableGLCap(GL_BLEND);
+        engine.setBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+        engine.enableGLCap(GL_DEPTH_TEST);
+
+        Matrix4 oldModelView = engine.getModelviewMatrix();
+        Matrix4 modelView = Matrix4.get().initTranslation(-0.5f, -1, zoom)
+                .mul(Matrix4.get().initTranslation(0.5f, 0, 0.5f)
+                        .mul(Matrix4.get().initTranslation(transX, transY, transZ))
+                        .mul(new Quaternion(Vector3.xAxis, xAxis)
+                                .mul(new Quaternion(Vector3.yAxis, yAxis)).toRotationMatrix())
+                        .mul(Matrix4.get().initTranslation(-0.5f, 0, -0.5f)));
+        engine.setModelviewMatrix(modelView);
+        engine.bindLocation(new ResourceLocation("tecbula", "textures/grid.png"));
+        engine.renderBuffer(buff);
+        oc.getRenderItems().renderItem(engine, rootStack, null, 0, -1, 0);
+
+        for(ModelBox box : currentModel.getChildren())
+        {
+            if(box == null)
+                continue;
+            if(!buffers.containsKey(box))
+            {
+                OpenGLBuffer buffer = new OpenGLBuffer();
+                box.prepareBuffer(texture, buffer);
+                buffers.put(box, buffer);
+            }
+            Matrix4 rot = box.getRotation().toRotationMatrix();
+            Matrix4 translation = Matrix4.get().initTranslation(0, 0, 0);
+            Quaternion erot = new Quaternion(Vector3.yAxis, 0);
+            Matrix4 rot1 = erot.toRotationMatrix();
+
+            Matrix4 finalMatrix = (rot.mul(rot1));
+            engine.setModelviewMatrix(modelView.mul(finalMatrix));
+            engine.renderBuffer(buffers.get(box), texture);
+            translation.dispose();
+            rot1.dispose();
+            rot.dispose();
+            finalMatrix.dispose();
+        }
+
+        modelView.dispose();
+        engine.setModelviewMatrix(oldModelView);
+        engine.disableGLCap(GL_ALPHA_TEST);
+        engine.disableGLCap(GL_DEPTH_TEST);
+        engine.end();
+    }
+
+    public void handleButtonReleased(int x, int y, int button)
+    {
+        super.handleButtonReleased(x, y, button);
+        if(button == 1)
+        {
+            rightMousePressed = false;
+        }
+        else if(button == 2)
+        {
+            middleMousePressed = false;
+        }
+
+    }
+
+    public void handleMouseWheelMovement(int mx, int my, int deltaWheel)
+    {
+        super.handleMouseWheelMovement(mx, my, deltaWheel);
+        zoom -= deltaWheel / 120f;
+    }
+
+    public void handleButtonPressed(int x, int y, int button)
+    {
+        super.handleButtonPressed(x, y, button);
+        if(button == 1)
+        {
+            rightMousePressed = true;
+        }
+        else if(button == 2)
+        {
+            middleMousePressed = true;
+        }
+    }
+
+    public void handleMouseMovement(int x, int y, int dx, int dy)
+    {
+        super.handleMouseMovement(x, y, dx, dy);
+        if(rightMousePressed)
+        {
+            yAxis -= Math.toRadians(dx);
+            xAxis += Math.toRadians(dy);
+        }
+        if(middleMousePressed)
+        {
+            transY += dy / 20f;
+            transX += dx / 20f;
+        }
+    }
+
+}
